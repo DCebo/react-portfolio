40b910854f237b94133eca75ef31b6666688980d
DCebo <d.ceballos575@gmail.com>
2020-09-21 09:35:20 -0400

    Updates

---
+++ precache-manifest.8bed4403c0b1e773d1e5be1550348f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd4f3db940ecf80a7cb2eff1caac297",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d730ac3c5c19c5caae7",
+    "url": "./static/css/main.d8511f0b.chunk.css"
+  },
+  {
+    "revision": "26ef2219c6f54322fd0b",
+    "url": "./static/js/2.669ee9a8.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "./static/js/2.669ee9a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d730ac3c5c19c5caae7",
+    "url": "./static/js/main.24513590.chunk.js"
+  },
+  {
+    "revision": "1436ac51eb3085ff3d5c",
+    "url": "./static/js/runtime-main.c2538a3a.js"
+  }
+]);
